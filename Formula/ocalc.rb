edce121147d95d977e6add8b38399e1e3bea690f
Ľubomír Kurčák <29227697+lkurcak@users.noreply.github.com>
2025-11-11 03:40:09 +0000

    ocalc: add 0.1.0 bottle.

--- Formula/ocalc.rb
+++ Formula/ocalc.rb
@@ -6,6 +6,12 @@ class Ocalc < Formula
   license "Unlicense"
   head "https://github.com/lkurcak/ocalc.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/lkurcak/homebrew-tap/releases/download/ocalc-0.1.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "82038433f188c40d533757ca3299d703ea0cb93a686e3ed9001c87c3f580555d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f9d8b6f6544a1b81b79cbe44b0246ed69a044c098cf4b88cba9dac5160c55548"
+  end
+
   depends_on "dune" => :build
   depends_on "ocaml"
 

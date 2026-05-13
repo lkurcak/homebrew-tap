dabaecb968620557f502a3b6301332ef4f07ca8e
Ľubomír Kurčák <29227697+lkurcak@users.noreply.github.com>
2026-05-13 13:49:26 +0000

    tend: update 1.0.0 bottle.

--- Formula/tend.rb
+++ Formula/tend.rb
@@ -12,9 +12,9 @@ class Tend < Formula
   end
 
   bottle do
-    root_url "https://github.com/lkurcak/homebrew-tap/releases/download/tend-0.2.21"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "29e56f37de2614713a6fc57cba27d0141a474caf1db940656b7066c6380c6ea2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c14d4dc17ca6c6c143d19f5e8d93d6fd56b6b3124293cff28995090a9ecf0fdc"
+    root_url "https://github.com/lkurcak/homebrew-tap/releases/download/tend-1.0.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b370ae8e2ab38ed4930e0b54cadeac88d0ec67b5fbe304b80fcf465d0bb3b953"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7ed90dbe8b76bfc65187d17b58df781a0b29d09553c5a23220178129b823ee8a"
   end
 
   depends_on "rust" => :build

29461d265dd6d6903890a1a5b271e24e944de46a
Ľubomír Kurčák <29227697+lkurcak@users.noreply.github.com>
2026-05-27 14:29:16 +0000

    ghelpr: add 0.2.3 bottle.

--- Formula/ghelpr.rb
+++ Formula/ghelpr.rb
@@ -11,6 +11,13 @@ class Ghelpr < Formula
     strategy :github_latest
   end
 
+  bottle do
+    root_url "https://github.com/lkurcak/homebrew-tap/releases/download/ghelpr-0.2.3"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9c6ea44ccb49cae29b936aa554036a456ce951ca4e29f66cc5b27966b8e6c72a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e542f6ec2db37c9cecaffa6eeb775364e0f848dd56df050ed32f1d195cd3c9fd"
+  end
+
   depends_on "rust" => :build
 
   def install

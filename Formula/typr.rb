ce7092fca5298cacdb58e6f9298085d0b9659638
Ľubomír Kurčák <29227697+lkurcak@users.noreply.github.com>
2026-09-05 03:38:59 +0000

    typr: add 0.2.7 bottle.

--- Formula/typr.rb
+++ Formula/typr.rb
@@ -7,6 +7,12 @@ class Typr < Formula
     skip "The upstream source repository is private"
   end
 
+  bottle do
+    root_url "https://github.com/lkurcak/homebrew-tap/releases/download/typr-0.2.7"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "042a576e666f96a8bc45fc10cb8551617a4457f89f0ecba846e293188ff7d711"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "3efa6387a1da6e4a28f447b96cbbb04838796b01d76eb33d45e24a9b221674ea"
+  end
+
   on_macos do
     on_arm do
       url "https://github.com/lkurcak/homebrew-tap/releases/download/typr-dist-v0.2.7/typr-v0.2.7-aarch64-apple-darwin.tar.gz"
